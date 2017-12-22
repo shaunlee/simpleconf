e61819aed2d1036eff80e96c59c301a05f840696
Shaun Li <shonhen@gmail.com>
2017-12-22 09:02:11 +0800

    ignore failed peers

--- peers/peers.go
+++ peers/peers.go
@@ -1,13 +1,9 @@
 package peers
 
 import (
-	"errors"
 	"github.com/json-iterator/go"
 	"github.com/kataras/iris"
-	"github.com/parnurzeal/gorequest"
 	"github.com/shaunlee/simpleconf/models"
-	"log"
-	"time"
 )
 
 var (
@@ -56,89 +52,6 @@ func rewrite_aof(ctx iris.Context) {
 	ctx.JSON(iris.Map{
 		"ok": true,
 	})
-}
-
-func Restore(peers []string) error {
-	if len(peers) == 0 {
-		return nil
-	}
-
-	for _, addr := range peers {
-		url := addr + "/db"
-		log.Println("trying to restore from", url)
-		if resp, body, err := (gorequest.New().Timeout(2 * time.Second)).Get(url).End(); err != nil {
-			log.Println("failed to restore", err)
-			continue
-		} else {
-			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-				log.Println("failed to restore", resp.Status, body)
-				continue
-			}
-
-			models.Configuration = body
-			models.RewriteAof()
-			return nil
-		}
-	}
-
-	return errors.New("There is no valid peer to be restore from")
-}
-
-func SyncUpdate(key string, value interface{}) error {
-	for _, addr := range peers {
-		url := addr + "/db/" + key
-		v, _ := json.Marshal(value)
-		if resp, _, err := (gorequest.New().Timeout(2 * time.Second)).Post(url).Send(string(v)).End(); err != nil {
-			return err[0]
-		} else {
-			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-				return errors.New(resp.Status)
-			}
-		}
-	}
-	return nil
-}
-
-func SyncDelete(key string) error {
-	for _, addr := range peers {
-		url := addr + "/db/" + key
-		if resp, _, err := (gorequest.New().Timeout(2 * time.Second)).Delete(url).End(); err != nil {
-			return err[0]
-		} else {
-			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-				return errors.New(resp.Status)
-			}
-		}
-	}
-	return nil
-}
-
-func SyncClone(fromKey, toKey string) error {
-	for _, addr := range peers {
-		url := addr + "/clone/" + fromKey + "/" + toKey
-		if resp, _, err := (gorequest.New().Timeout(2 * time.Second)).Post(url).End(); err != nil {
-			return err[0]
-		} else {
-			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-				return errors.New(resp.Status)
-			}
-		}
-	}
-	return nil
-}
-
-func SyncRewriteAof() error {
-	for _, addr := range peers {
-		url := addr + "/rewriteaof"
-		if resp, _, err := (gorequest.New().Timeout(2 * time.Second)).Post(url).End(); err != nil {
-			return err[0]
-		} else {
-			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-				return errors.New(resp.Status)
-			}
-		}
-	}
-	return nil
 }
 
 func Listen(addr string, peerAddrs []string) {
---
+++ peers/sync.go
@@ -0,0 +1,88 @@
+package peers
+
+import (
+	"errors"
+	"github.com/parnurzeal/gorequest"
+	"github.com/shaunlee/simpleconf/models"
+	"log"
+	"time"
+)
+
+func Restore(peers []string) error {
+	if len(peers) == 0 {
+		return nil
+	}
+
+	for _, addr := range peers {
+		url := addr + "/db"
+		log.Println("trying to restore from", url)
+		if resp, body, err := (gorequest.New().Timeout(2 * time.Second)).Get(url).End(); err != nil {
+			log.Println("failed to restore", err)
+			continue
+		} else {
+			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+				log.Println("failed to restore", resp.Status, body)
+				continue
+			}
+
+			models.Configuration = body
+			models.RewriteAof()
+			return nil
+		}
+	}
+
+	return errors.New("There is no valid peer to be restore from")
+}
+
+func SyncUpdate(key string, value interface{}) {
+	for _, addr := range peers {
+		url := addr + "/db/" + key
+		v, _ := json.Marshal(value)
+		if resp, _, err := (gorequest.New().Timeout(2 * time.Second)).Post(url).Send(string(v)).End(); err != nil {
+			log.Println("failed to sync", url, err)
+		} else {
+			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+				log.Println("failed to sync", url, resp.Status)
+			}
+		}
+	}
+}
+
+func SyncDelete(key string) {
+	for _, addr := range peers {
+		url := addr + "/db/" + key
+		if resp, _, err := (gorequest.New().Timeout(2 * time.Second)).Delete(url).End(); err != nil {
+			log.Println("failed to sync", url, err)
+		} else {
+			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+				log.Println("failed to sync", url, resp.Status)
+			}
+		}
+	}
+}
+
+func SyncClone(fromKey, toKey string) {
+	for _, addr := range peers {
+		url := addr + "/clone/" + fromKey + "/" + toKey
+		if resp, _, err := (gorequest.New().Timeout(2 * time.Second)).Post(url).End(); err != nil {
+			log.Println("failed to sync", url, err)
+		} else {
+			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+				log.Println("failed to sync", url, resp.Status)
+			}
+		}
+	}
+}
+
+func SyncRewriteAof() {
+	for _, addr := range peers {
+		url := addr + "/rewriteaof"
+		if resp, _, err := (gorequest.New().Timeout(2 * time.Second)).Post(url).End(); err != nil {
+			log.Println("failed to sync", url, err)
+		} else {
+			if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+				log.Println("failed to sync", url, resp.Status)
+			}
+		}
+	}
+}
