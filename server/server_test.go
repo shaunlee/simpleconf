a5dc5fc67afcb791b2f1feaff2b3f9dc1d3b395a
Shaun <me@shaunli.com>
2025-08-08 16:01:20 +0800

    test tcp server

--- server/server_test.go
+++ server/server_test.go
@@ -17,15 +17,17 @@ func init() {
 	conn = nc
 	reader = bufio.NewReader(conn)
 
-	fmt.Fprintf(conn, "+bench\n\"mark\"\n")
-	reader.ReadBytes('\n')
+	//fmt.Fprintf(conn, "+bench\n\"mark\"\n")
+	//v, _ := reader.ReadBytes('\n')
+	//fmt.Print(string(v))
+	//v, _ = reader.ReadBytes('\n')
+	//fmt.Print(string(v))
 }
 
-func BenchmarkTcpGet(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		fmt.Fprintf(conn, "=bench\n")
-		reader.ReadBytes('\n')
-	}
+func TestTcpSet(t *testing.T) {
+	fmt.Fprintf(conn, "+bench\n\"mark\"\n")
+	v, _ := reader.ReadBytes('\n')
+	fmt.Print(string(v))
 }
 
 func BenchmarkTcpSet(b *testing.B) {
@@ -35,16 +37,44 @@ func BenchmarkTcpSet(b *testing.B) {
 	}
 }
 
-func BenchmarkTcpDel(b *testing.B) {
+func TestTcpGet(t *testing.T) {
+	fmt.Fprintf(conn, "=\n")
+	v, _ := reader.ReadBytes('\n')
+	fmt.Print(string(v))
+	v, _ = reader.ReadBytes('\n')
+	fmt.Print(string(v))
+}
+
+func BenchmarkTcpGet(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		fmt.Fprintf(conn, "-bench\n")
+		fmt.Fprintf(conn, "=bench\n")
+		reader.ReadBytes('\n')
 		reader.ReadBytes('\n')
 	}
+}
+
+func TestTcpClone(t *testing.T) {
+	fmt.Fprintf(conn, "<bench\n>mark\n")
+	v, _ := reader.ReadBytes('\n')
+	fmt.Print(string(v))
 }
 
 func BenchmarkTcpClone(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		fmt.Fprintf(conn, "<bench\n>mark\n")
+		reader.ReadBytes('\n')
+	}
+}
+
+func TestTcpDel(t *testing.T) {
+	fmt.Fprintf(conn, "-bench\n")
+	v, _ := reader.ReadBytes('\n')
+	fmt.Print(string(v))
+}
+
+func BenchmarkTcpDel(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fmt.Fprintf(conn, "-bench\n")
 		reader.ReadBytes('\n')
 	}
 }
